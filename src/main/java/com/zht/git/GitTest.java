41f220b73472b2b373acab5ad6d6e2f760cbd1fe
zhtt123 <104259345+zhtt123@users.noreply.github.com>
2022-05-11 17:44:17 +0800

    Update GitTest.java

--- src/main/java/com/zht/git/GitTest.java
+++ src/main/java/com/zht/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello master!");
         System.out.println("hello login!");
         System.out.println("hello github!");
+        System.out.println("hello pull!");
     }
 }
