d2e33ffa6881f41d4cfb3b03041fcdd7d1f219e0
zht <1683412300@qq.com>
2022-05-11 16:41:22 +0800

    second

--- src/main/java/com/zht/git/GitTest.java
+++ src/main/java/com/zht/git/GitTest.java
@@ -3,5 +3,6 @@ package com.zht.git;
 public class GitTest {
     public static  void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello world!");
     }
 }
