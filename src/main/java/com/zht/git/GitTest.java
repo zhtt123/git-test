0f0c08b5665170ae24b2d45cfe23271330f542c9
zht <1683412300@qq.com>
2022-05-11 17:34:28 +0800

    four

--- src/main/java/com/zht/git/GitTest.java
+++ src/main/java/com/zht/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello world2!");
         System.out.println("hello master!");
         System.out.println("hello login!");
+        System.out.println("hello github!");
     }
 }
